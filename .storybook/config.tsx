c3cecd84177152b110c39d604b080b92bc02ed4c
jgchenu <jgchenu@foxmail.com>
2020-04-13 00:20:18 +0800

    fix: 去除fontawasom

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,10 +1,7 @@
 import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import React from "react";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
 import "../src/styles/index.scss";
-library.add(fas);
 const wrapperStyle: React.CSSProperties = {
   padding: "20px 40px",
 };
